a27a9cebd69fbf5c5e7018e38db30c461e4d2454
Jam Risser <jam@codejam.ninja>
2019-11-30 06:22:45 +0000

    Improved depricated project

--- dotstow/__main__.py
+++ dotstow/__main__.py
@@ -1,6 +1,45 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from dotstow import App
+import warnings
+
+warnings.warn('''
+The pypi dotstow module is deprecated.
+
+
+Please use the one found on npm.
+https://npmjs.org/package/dotstow
+
+You can install it by running the following command.
+
+```
+npm install -g dotstow
+```
+
+
+The new and improved dotstow module on npm
+supports  the following features.
+
+* Group dotfiles into units (stow packages)
+* Automatically symlink (stow) files
+* Backup dotfiles with git
+* Keep track of simultaneous dotfile configurations for multiple environments
+* Supports shell autocompletion
+
+
+If you were using the python version of dotstow, you should upgrade to this
+version to get the benefits of multiple environments. If you switch you will
+have to move your stash plugins into an environment folder (global is recommended).
+
+You can do that by running the following commands.
+
+```
+mkdir ~/tmp_global
+mv ~/.dotfiles/* ~/tmp_global
+mv ~/tmp_global mkdir ~/.dotfiles/global
+```
+''', DeprecationWarning,
+              stacklevel=2)
 
 def main():
     with App() as app:
