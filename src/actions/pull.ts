599afb7396fbea38108ad8608a5b6d38be1bcd91
Jam Risser <jam@codejam.ninja>
2020-09-11 18:17:42 +0000

    Added basic pull command

---
+++ src/actions/pull.ts
@@ -0,0 +1,28 @@
+import fs from 'fs-extra';
+import ora from 'ora';
+import path from 'path';
+import { homedir } from 'os';
+import { Git } from '../services';
+import { Options } from '../types';
+
+export default async function pull(options: Options = {}): Promise<any> {
+  const dotfilesPath = path.resolve(homedir(), options.dotfiles || '.dotfiles');
+  options.dotfiles = dotfilesPath;
+  const spinner = ora();
+  const git = new Git(options);
+  try {
+    if (await fs.pathExists(dotfilesPath)) {
+      spinner.start('pulling dotfiles');
+      await git.pull();
+      spinner.succeed('pulled dotfiles');
+    } else {
+      const remote = options.remote || (await git.guessRemote());
+      spinner.start('cloning dotfiles');
+      await git.clone(remote);
+      spinner.succeed('cloned dotfiles');
+    }
+  } catch (err) {
+    if (options.debug) throw err;
+    spinner.fail(err.message);
+  }
+}
