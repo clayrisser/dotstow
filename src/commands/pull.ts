4470b53a8500f7596717aaff9ae8035ec9d30a50
Sreeraj Karichery <sreerajmylatty@gmail.com>
2020-09-29 10:02:56 +0530

    Support bootstrap.yml config

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -8,13 +8,13 @@ export default class Pull extends Command {
   static examples = ['$ dotstow pull'];
 
   static flags = {
-    debug: flags.boolean({ char: 'd', required: false }),
+    debug: flags.boolean({ char: 'd', required: false })
   };
 
   async run() {
     const { flags } = this.parse(Pull);
     const options: Options = {
-      debug: !!flags.debug,
+      debug: !!flags.debug
     };
     return pull(options);
   }
